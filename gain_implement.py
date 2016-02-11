6df34d7980d3ff92b39b87d481614e0f90474fff
Pedro Bemerguy <pedrobemerguy96@gmail.com>
2016-02-11 13:16:55 -0300

    Implement stylee PEP 08 guide

--- gain_implement.py
+++ gain_implement.py
@@ -9,32 +9,26 @@ import signal
 
 class bar_class():
 
-    def bar(self,loader,bar_progress_l,bar_empty_l,g_l,time_total):
-        
-        sys.stdout.write("\r[%s%s%s] Current Gain:%f / Time running: %0.3f "%\
-                                           (bar_progress_l,bar_empty_l,loader,g_l,time_total))
+    def bar(self, loader, bar_progress_l, bar_empty_l, g_l, time_total):    
+        sys.stdout.write("\r[%s%s%s] Current Gain: %f / Time running: %0.3f "%\
+                        (bar_progress_l, bar_empty_l, loader,g_l, time_total))
         sys.stdout.flush();
         time.sleep(0.05)
 
-
-    def loader(self,bar_progress_p,bar_empty_p,g_p,time_trans):
-          time_init_loader = time.time()
-          while True:
+    def loader(self, bar_progress_p, bar_empty_p, g_p, time_trans):
+        time_init_loader = time.time()
+        while True:
             for i in range(1,5):
                 dict = {
-                      1: self.bar('-',bar_progress_p,bar_empty_p,g_p,time_trans+time.time()-time_init_loader),
-                      2: self.bar('/',bar_progress_p,bar_empty_p,g_p,time_trans+time.time()-time_init_loader),
-                      3: self.bar('|',bar_progress_p,bar_empty_p,g_p,time_trans+time.time()-time_init_loader),
-                      4: self.bar('\\',bar_progress_p,bar_empty_p,g_p,time_trans+time.time()-time_init_loader)
-                }
-
-    
-
-      
-
-
-
-
+                        1: self.bar('-', bar_progress_p, bar_empty_p, g_p,\
+                                   time_trans + time.time() - time_init_loader),
+                        2: self.bar('/', bar_progress_p, bar_empty_p, g_p,\
+                                   time_trans + time.time() - time_init_loader),
+                        3: self.bar('|', bar_progress_p, bar_empty_p, g_p,\
+                                   time_trans + time.time() - time_init_loader),
+                        4: self.bar('\\', bar_progress_p, bar_empty_p, g_p,\
+                                   time_trans + time.time() - time_init_loader)
+                        }
 
 
 if __name__ == '__main__':
@@ -46,14 +40,16 @@ if __name__ == '__main__':
 
     if args.gain[0] > args.gain[1]:
         i = -1
-        gain_list=range(args.gain[0],args.gain[1]+i,i)
+        gain_list = range(args.gain[0], args.gain[1] + i, i)
         try:
-          time_init=time.time()
-          m_bar_class = bar_class()
-          for g in gain_list:
-                bar_progress = ((args.gain[0]-g)*'#')
-                bar_empty = ' '*(g-args.gain[1])
-                p = multiprocessing.Process(target=m_bar_class.loader,args=(bar_progress,bar_empty,g,time.time()-time_init,))
+            time_init = time.time()
+            m_bar_class = bar_class()
+            for g in gain_list:
+                bar_progress = ((args.gain[0]-g) * '#')
+                bar_empty = ' ' * (g-args.gain[1])
+                p = multiprocessing.Process(target=m_bar_class.loader,\
+                                            args=(bar_progress, bar_empty, g,\
+                                                 time.time() - time_init,))
                 p.start()
                 time.sleep(time_gain)
                 p.terminate()
@@ -61,24 +57,28 @@ if __name__ == '__main__':
             pass 
         
         sys.stdout.write("\r[%s%s] Current Gain:%f / Time running: %0.3f \n"%\
-                                ('#'*(args.gain[0]-args.gain[1]-i),' '*0,g,time.time()-time_init))
+                        ('#' * (args.gain[0]-args.gain[1]-i), ' ' * 0, g,\
+                         time.time() - time_init))
 
     elif args.gain[0] < args.gain[1]:
         i = 1
-        gain_list=range(args.gain[0],args.gain[1]+i,i)
+        gain_list = range(args.gain[0], args.gain[1] + i, i)
         try:
-          time_init=time.time()
-          for g in gain_list:
-                bar_progress =(g-args.gain[0])*'#'
-                bar_empty = ' '*(args.gain[1]-g)
-                p = multiprocessing.Process(target=m_bar_class.loader,args=(bar_progress,bar_empty,g,time.time()-time_init,))
+            time_init=time.time()
+            for g in gain_list:
+                bar_progress = (g-args.gain[0]) * '#'
+                bar_empty = ' ' * (args.gain[1]-g)
+                p = multiprocessing.Process(target=m_bar_class.loader,\
+                                            args=(bar_progress, bar_empty, g,\
+                                                 time.time() - time_init,))
                 p.start()
                 time.sleep(time_gain)
                 p.terminate()
         except [[KeyboardInterrupt]]:
             pass
         sys.stdout.write("\r[%s%s] Current Gain:%f / Time running: %0.3f \n"%\
-                                ('#'*(args.gain[1]-args.gain[0]+i),' '*0,g,time.time()-time_init))
+                        ('#' * (args.gain[1]-args.gain[0] + i), ' ' * 0 , g,\
+                        time.time() - time_init))
 
     else:
         print "The gain need to have different values"
